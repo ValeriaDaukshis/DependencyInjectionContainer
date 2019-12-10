e8c1dbd1fcb63326424ea7f0eb00d8799bb720f5
Valeria <daukshislera2000@gmail.com>
2019-12-11 00:22:23 +0300

    add resolve method

--- DIContainer/DependenciesConfiguration.cs
+++ DIContainer/DependenciesConfiguration.cs
@@ -6,10 +6,10 @@ namespace DIContainer
 {
     public class DependenciesConfiguration
     {
-        private ConcurrentDictionary<Type, List<ImplementationInfo>> _dependencesList;
+        public ConcurrentDictionary<Type, List<ImplementationInfo>> DependencesList { get; set; }
         public DependenciesConfiguration()
         {
-            _dependencesList = new ConcurrentDictionary<Type, List<ImplementationInfo>>();
+            DependencesList = new ConcurrentDictionary<Type, List<ImplementationInfo>>();
         }
 
         public void Register<TDependency, TImplementation>(bool singleTone = false)
@@ -20,14 +20,14 @@ namespace DIContainer
         public void Register(Type dependency, Type implementation, bool singleTone = false)
         {
             ImplementationInfo implementationInfo = new ImplementationInfo(implementation, singleTone);
-            if (_dependencesList.ContainsKey(implementation))
+            if (DependencesList.ContainsKey(implementation))
             {
-                _dependencesList[implementation].Add(implementationInfo);
+                DependencesList[implementation].Add(implementationInfo);
             }
             else
             {
-                _dependencesList.TryAdd(dependency, new List<ImplementationInfo>());
-                _dependencesList[dependency].Add(implementationInfo);
+                DependencesList.TryAdd(dependency, new List<ImplementationInfo>());
+                DependencesList[dependency].Add(implementationInfo);
             }
         }
     }
--- DIContainer/DependencyProvider.cs
+++ DIContainer/DependencyProvider.cs
@@ -1,12 +1,95 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DIContainer
 {
     public class DependencyProvider
     {
+        private DependenciesConfiguration _configuration;
+        public DependencyProvider(DependenciesConfiguration configuration)
+        {
+            if(ValidateConfiguration(configuration))
+            {
+                this._configuration = configuration;
+            }
+            else
+            {
+                throw new ArgumentException("Config is not valid");
+            }
+        }
+        
+        private bool ValidateConfiguration(DependenciesConfiguration dependenciesConfiguration)
+        {
+            foreach (Type dependences in dependenciesConfiguration.DependencesList.Keys)
+            {
+                if (!dependences.IsValueType)
+                {
+                    foreach (ImplementationInfo dependency in dependenciesConfiguration.DependencesList[dependences])
+                    {
+                        Type implementation = dependency.ImplementationType;
+
+                        if (implementation.IsAbstract || implementation.IsInterface || !dependences.IsAssignableFrom(implementation))
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        public T Resolve<T>() where T: class
+        {
+            Type t = typeof(T);
+
+            return (T)Resolve(t);
+        }
+        
+        private object Resolve(Type type)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+            {
+                return CreateGeneric(type);
+            }
+            
+            _configuration.DependencesList.TryGetValue(type, out List<ImplementationInfo> implementations);
+            if (implementations != null)
+                return GetInstance(implementations.First());
+
+            if (type.IsGenericType)
+            {
+                var genericDefinition = type.GetGenericTypeDefinition();                    
+                _configuration.DependencesList.TryGetValue(genericDefinition, out implementations);
+                if (implementations != null)
+                    return GetInstance(implementations.First());
+            }
+            
+            throw new ArgumentException("Unknown type " + type.Name);
+            
+        }
         public object Create(Type type)
         {
             return null;
+        }
+
+        private object CreateGeneric(Type type)
+        {
+            return null;
+        }
+        
+        private object GetInstance(ImplementationInfo tImplementation)
+        {
+            if (tImplementation.SingleTone)
+            {
+                return tImplementation.SingleToneRealization.GetInstance(this);
+            }
+            
+            return Create(tImplementation.ImplementationType);
         }
     }
 }
