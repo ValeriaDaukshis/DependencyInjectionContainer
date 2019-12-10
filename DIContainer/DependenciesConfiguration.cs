4f06b854249f4a84cdce255661b272f8481d3a26
Valeria <daukshislera2000@gmail.com>
2019-12-10 23:45:05 +0300

    Configuration

---
+++ DIContainer/DependenciesConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace DIContainer
+{
+    public class DependenciesConfiguration
+    {
+        private ConcurrentDictionary<Type, List<ImplementationInfo>> _dependencesList;
+        public DependenciesConfiguration()
+        {
+            _dependencesList = new ConcurrentDictionary<Type, List<ImplementationInfo>>();
+        }
+
+        public void Register<TDependency, TImplementation>(bool singleTone = false)
+        {
+            Register(typeof(TDependency), typeof(TImplementation), singleTone);
+        }
+        
+        public void Register(Type dependency, Type implementation, bool singleTone = false)
+        {
+            ImplementationInfo implementationInfo = new ImplementationInfo(implementation, singleTone);
+            if (_dependencesList.ContainsKey(implementation))
+            {
+                _dependencesList[implementation].Add(implementationInfo);
+            }
+            else
+            {
+                _dependencesList.TryAdd(dependency, new List<ImplementationInfo>());
+                _dependencesList[dependency].Add(implementationInfo);
+            }
+        }
+    }
+}
---
+++ DIContainer/ImplementationInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DIContainer
+{
+    public class ImplementationInfo
+    {
+        public Type implementationType { get; }
+        public bool singleTone { get; }  
+        
+        public ImplementationInfo(Type t, bool singleTone)
+        {
+            this.singleTone = singleTone;
+            this.implementationType = t;
+        }
+    }
+}
